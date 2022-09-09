34753f7a217cc710d16272087c413a00ab853e1a
サジッド(sazid) <mdip@uxd.co.jp>
2022-09-09 16:54:01 +0900

    Initial Commit

---
+++ iPhone_Linux_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iPhone_Linux_Project
+//
+//  Created by UXD on 2022/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iPhone_Linux_Project/iPhone_Linux_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhone_Linux_ProjectApp.swift
+//  iPhone_Linux_Project
+//
+//  Created by UXD on 2022/09/09.
+//
+
+import SwiftUI
+
+@main
+struct iPhone_Linux_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
