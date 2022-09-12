eb97bfb88a7f05d2b53953814f57ba77712b03b3
サジッド(sazid) <mdip@uxd.co.jp>
2022-09-12 11:33:56 +0900

    first commit

--- iPhone_Linux_Project/ContentView.swift
+++ iPhone_Linux_Project/ContentView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var textEditorText:String="This is the sample text."
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView{
+            VStack{
+                TextEditor(text:$textEditorText)
+                Button(action: {
+                    
+                }, label: {
+                    Text("Display".uppercased())
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .padding()
+                        .padding(.horizontal,10)
+                        .background(
+                            Capsule()
+                                .fill(Color.green)
+                        )
+                })
+            }
             .padding()
+            .background(Color.gray)
+            .navigationTitle("Welcome!")
+            .frame(maxHeight:.infinity, alignment:.bottom)
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
