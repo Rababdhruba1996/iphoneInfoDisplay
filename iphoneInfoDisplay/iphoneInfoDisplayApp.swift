557f21fda880ec8ccdb1dee344ae8bc8190db17c
サジッド(sazid) <mdip@uxd.co.jp>
2022-10-12 17:50:07 +0900

    Initial Commit

---
+++ iphoneInfoDisplay/iphoneInfoDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  iphoneInfoDisplayApp.swift
+//  iphoneInfoDisplay
+//
+//  Created by UXD on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct iphoneInfoDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
